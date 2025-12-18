63a590626b7a5d2ee6a84e8a9a9dd2b43875abfd
Naman1002 <58694695+Naman1002@users.noreply.github.com>
2025-12-18 11:55:23 +0000

    array chapter

--- fundamentals/arrays.js
+++ fundamentals/arrays.js
@@ -5,7 +5,7 @@ let shoppingCart = fruits;
 shoppingCart.push("Banana");
 
 // what's in fruits?
-// console.log( fruits.length ); 4
+console.log( fruits.length ); 4
 
 styles = ["Jazz", "Blues"]
 styles.push("Rock-n-Roll");
@@ -28,3 +28,53 @@ const maxArr = (arrNum) =>{
             } 
         }
 }
+
+
+arr = [2,3,4,-1,1,-1]
+    if(arr[3] <  0 ){
+        let sum =0;
+        let newArr = []
+        for(i = 0; i < arr.length; i++){
+           sum += arr[i]; 
+           newArr.push(sum);
+        }
+       newArr
+
+        console.log(`The sum is ${newArr[(newArr.length)-1]} and the maximal array is ${newArr}`) ;
+    }
+
+
+const maximalArray = (arr) =>{
+    sec_arr = [2,3,4,-1,1,-1,-2];
+           let sumArr =0;
+let biggestSum = []
+        for(i = 0; i < sec_arr.length; i++){
+           sumArr += sec_arr[i]; 
+           biggestSum.push(sumArr);
+        }
+        console.log(biggestSum)
+        let max = biggestSum[0];
+        for(i=0; i<biggestSum.length ; i++){
+            if (biggestSum[i] > max){
+                max = biggestSum[i];
+            }
+        }
+
+        console.log(max)
+        return max;
+
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
